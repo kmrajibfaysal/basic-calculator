fc009449cac8578ce461af1219d3637aa1234fba
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-02-10 21:18:44 +0600

    adding style to equal and clear button

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 const buttons = document.querySelectorAll('button');
 for (const button of buttons) {
-    button.classList.add('border-2', 'border-lime-400');
+    button.classList.add('button-style');
 }
